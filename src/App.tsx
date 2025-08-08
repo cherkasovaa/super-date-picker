e924a50f7d7e951e601575b91c9591f92acebf49
cherkasovaa <alina.ch1202@gmail.com>
2025-08-08 14:05:56 +0300

    feat: project setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import styles from './App.module.css';
+
+function App() {
+  return <p className={styles.wrapper}>You will see a date picker here soon</p>;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
+    },
+  },
+});
