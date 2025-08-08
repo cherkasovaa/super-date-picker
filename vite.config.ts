1e93812475689baab3af92904a01f81826cad19b
cherkasovaa <alina.ch1202@gmail.com>
2025-08-08 16:05:39 +0300

    chore: path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svgr({ include: '**/*.svg?react' }), react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
