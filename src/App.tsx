24c82c181476fe9edf4b420396bcf4bf2e15c518
cherkasovaa <alina.ch1202@gmail.com>
2025-08-11 11:12:30 +0300

    feat: integrate SuperDatePicker for demonstration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import type { DateRange } from '@/shared/types/date';
+import { SuperDatePicker } from '@/widgets/super-date-picker';
+import { useState } from 'react';
 import styles from './App.module.css';
 
 function App() {
-  return <p className={styles.wrapper}>You will see a date picker here soon</p>;
+  const [range, setRange] = useState<DateRange>({ from: 'now', to: 'now' });
+
+  return (
+    <div className={styles.wrapper}>
+      <SuperDatePicker value={range} onChange={setRange} />
+    </div>
+  );
 }
 
 export default App;
